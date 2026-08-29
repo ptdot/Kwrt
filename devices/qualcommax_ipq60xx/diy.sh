8bdbcafd64f2cb4affdff06b795a76bfa3f008cb
ptdot <92151608+ptdot@users.noreply.github.com>
2026-08-29 07:31:42 +0700

    Update diy.sh

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -22,7 +22,8 @@ rm -rf package/nss-packages/nss-userspace-oss
 
 sed -i "s/luci uboot-envtools wpad-openssl/luci uboot-envtools wpad-mbedtls/" target/linux/qualcommax/Makefile
 
-echo 'src-git amneziawg https://github.com/Slava-Shchipunov/awg-openwrt.git' >> feeds.conf.default
 
 echo "CONFIG_TARGET_qualcommax_ipq60xx_DEVICE_jdcloud_re-ss-01=y" >> .config
-echo 'src-git amneziawg https://github.com/amnezia-vpn/amneziawg-openwrt.git' >> feeds.conf.default
+grep -q "amneziawg" feeds.conf.default || echo 'src-git amneziawg https://github.com/Slava-Shchipunov/awg-openwrt.git' >> feeds.conf.default
+./scripts/feeds update amneziawg
+./scripts/feeds install -a -p amneziawg
